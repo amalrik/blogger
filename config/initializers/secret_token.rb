51a82cdef81fabb44e1698805d2d11e76e94e910
amalrik maia <amalrik.maia@gmail.com>
2012-07-29 21:38:48 -0300

    commit inicial com modelo articles criado e routes configurado.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'c94b4a0339620bb3b3c1be95473170c0683fa88e0d86e595ea1480660e8cc2a7be854de9cc8ec93919e03591f491017b825a55ca5c882c049acefc9f5a72d7f2'
