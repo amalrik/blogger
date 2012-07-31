46c0cc4a71b505a1838dfa3c0682d299c60e1798
amalrik maia <amalrik.maia@gmail.com>
2012-07-31 00:43:24 -0300

    refactoring da criacao de posts.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,9 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
-    @article.body  = params[:article][:body]
+    @article = Article.new(params[:article])
     @article.save
     redirect_to article_path(@article)
   end
