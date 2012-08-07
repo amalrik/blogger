c54d2f8829f2410573bebf0dd84e4828a0c5f344
amalrik maia <amalrik.maia@gmail.com>
2012-08-07 18:18:15 -0300

    gerado scaffold de login(author)

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
   # attr_accessible :title, :body
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Blogger::Application.routes.draw do
 
+  resources :authors
+
   root :to => 'articles#index'
   resources :tags
   resources :comments
