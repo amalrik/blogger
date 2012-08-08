09464ad064afc29fe8e571d413b7275b73ebc970
amalrik maia <amalrik.maia@gmail.com>
2012-08-08 18:04:07 -0300

    criados actions para login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Blogger::Application.routes.draw do
 
-  resources :authors
 
   root :to => 'articles#index'
   resources :tags
   resources :comments
   resources :articles
+
+  resources :author_sessions
+
+  match 'login'  => 'author_sessions#new',     :as => :login
+  match 'logout' => 'author_sessions#destroy', :as => :logout
 end
