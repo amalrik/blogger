e988fc4a8ede8535bd7b364ac49aecf1765d096c
amalrik maia <amalrik.maia@gmail.com>
2012-08-08 22:50:02 -0300

    melhorias na seguranca, somente usuarios auteticados podem executar tarefas administrativas

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,20 @@
 class TagsController < ApplicationController
+  before_filter :require_login, :only => [:destroy]
+
   def index
     @tags = Tag.all
   end
 
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    respond_to do |format|
+      format.html { redirect_to tags_url }
+    end
   end
 end
